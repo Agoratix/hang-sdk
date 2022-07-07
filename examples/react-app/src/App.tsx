566dd8945444e651c7e7c722ffc51903451fa429
Tobi Oduah <oduaht@gmail.com>
2022-07-07 15:53:41 -0700

    fix: take out router from example

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-import { Home, Coinbase } from './screens';
+import { Home } from './screens';
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/coinbase-sdk">Coinbase SDK</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/coinbase-sdk" element={<Coinbase />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <Home />;
 }
