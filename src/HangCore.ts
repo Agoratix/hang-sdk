114a4647b9d83a4de1b6ccd756a291c12ad203e5
Tobi Oduah <oduaht@gmail.com>
2022-07-14 17:33:53 -0700

    fix: update nftCost argument

--- src/HangCore.ts
+++ src/HangCore.ts
@@ -308,7 +308,7 @@ export class HangCore {
 
     connect(
       // @ts-ignore
-      { price: nftCost * quantity, count: quantity },
+      { price: String(nftCost * quantity), count: quantity },
       this.projectData?.collection_label,
       this.projectData?.info.title,
       this.projectData?.info.image
